f2ab01943fa127c23bd1b751adff4568d8db7f0f
Cindy Chen <chen_cx0128@hotmail.com>
2019-05-30 20:01:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,4 +11,5 @@ end
 puts "The line is currently empty."
 
 def take_a_number(katz_deli, name)
-  katz_deli
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
